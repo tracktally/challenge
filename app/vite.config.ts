7003b5e4fd584013d860415401a1bfa331ba16cd
Andrin Bertschi <hi@abertschi.ch>
2025-09-21 13:03:54 +0200

    add source map

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -5,6 +5,9 @@ import {VitePWA} from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
+    build: {
+        sourcemap: true,
+    },
     plugins: [react(),
         tailwindcss(),
         VitePWA({
